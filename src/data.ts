fb2b45a1d80d1926e437b3632cb38189ef64a32b
BBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-01-30 21:48:33 -0800

    Add more flashcards

--- src/data.ts
+++ src/data.ts
@@ -4,6 +4,7 @@ export const topics: models.Topic[] = [
     {
         title: "Lecture 2",
         flashcards: [
+            // 2.0.
             // What is an Operating System?
             {
                 question: "What is an Operating System?",
@@ -122,6 +123,139 @@ export const topics: models.Topic[] = [
                 question: "What role do drivers play in the I/O subsystem?",
                 answer: "Drivers in the I/O subsystem are responsible for specific hardware devices. They facilitate communication between the operating system and the hardware, ensuring proper functioning and interaction with the devices."
             },
+            // Examples of Operating Systems.
+            {
+                question: "What is the major concern of realtime OS?",
+                answer: "Meeting time constraints",
+            },
+            {
+                question: "What are examples of realtime OS?",
+                answer: "RT Linux, QNX, and Vxworks",
+            },
+            {
+                question: "What is an example of embedded OS?",
+                answer: "Contiki OS",
+            },
+            {
+                question: "What are examples of mobile OS?",
+                answer: "Android, iOS, and Windows Touch",
+            },
+            // 2.1
+            // Overview.
+            {
+                question: "What is a Virtual Machine Monitor (VMM) also known as?",
+                answer: "A hypervisor."
+            },
+            {
+                question: "What is the purpose of a Virtual Machine Monitor?",
+                answer: "The purpose of a VMM is to enable running multiple operating systems on a single physical machine."
+            },
+            {
+                question: "Can you provide examples of Virtual Machine Monitors?",
+                answer: "VMWare, Microsoft's VirtualPC/VirtualServer, Parallels (Macintosh), and Xen."
+            },
+            // System Models.
+            {
+                question: "What is a Type I Virtual Machine Monitor (VMM)?",
+                answer: "A Type I VMM runs directly on the raw hardware of a computer."
+            },
+            {
+                question: "What is a Type II Virtual Machine Monitor (VMM)?",
+                answer: "A Type II VMM runs hosted on another operating system, rather than directly on the raw hardware."
+            },
+            {
+                question: "When did virtualization first appear in IBM mainframes?",
+                answer: "Virtualization first appeared in IBM mainframes in 1972."
+            },
+            {
+                question: "What was the initial purpose of virtualization on IBM mainframes?",
+                answer: "It allowed multiple users to share a batch-oriented system."
+            },
+            {
+                question: "How did the formal definition of virtualization contribute to its expansion beyond IBM?",
+                answer: "The formal definition of virtualization specified that a Virtual Machine Monitor (VMM) provides an environment for programs that is essentially identical to the original machine. It also ensured that programs running within that environment show only minor performance decreases, and the VMM is in complete control of system resources."
+            },
+            {
+                question: "When did researchers start virtualizing on general-purpose PCs?",
+                answer: "In the late 1990s, Intel CPUs became fast enough for researchers to try virtualizing on general-purpose PCs."
+            },
+            {
+                question: "Which technologies, created in the late 1990s, are still used today for virtualization?",
+                answer: "Xen and VMware created technologies in the late 1990s, which are still used today for virtualization."
+            },
+            {
+                question: "How has virtualization expanded over time?",
+                answer: "Virtualization has expanded to many operating systems, CPUs, and Virtual Machine Monitors (VMMs)."
+            },
+            // Benefits and Features.
+            {
+                question: "How does virtualization protect the host system and VMs?",
+                answer: "The host system is protected from VMs, and VMs are protected from each other. This helps prevent the spread of viruses, and sharing is facilitated through shared file system volumes and network communication."
+            },
+            {
+                question: "What are some actions that can be performed on a running VM?",
+                answer: "Actions such as freeze, suspend, and running VM can be performed. VMs can be moved or copied to another location and then resumed. Snapshot functionality captures a given state, allowing restoration to that state. Some Virtual Machine Monitors (VMMs) enable multiple snapshots per VM. Cloning involves creating a copy and running both the original and the copy."
+            },
+            {
+                question: "What is a notable benefit of virtualization for OS research and system development efficiency?",
+                answer: "Virtualization is great for OS research and enhances system development efficiency."
+            },
+            {
+                question: "What is a practical use of running multiple, different OSes on a single machine?",
+                answer: "Running multiple, different operating systems on a single machine is useful for purposes such as consolidation and application development."
+            },
+            // Internet Services.
+            {
+                question: "What approach is suggested to achieve a rent-a-server economy for Internet services?",
+                answer: "The suggested approach is to use Virtual Machine Monitors (VMMs) to provide a rent-a-server economy, where users can rent virtual servers for hosting their services on the Internet."
+            },
+            // E2C.
+            {
+                question: "What does EC2 stand for?",
+                answer: "Amazon's Elastic Compute Cloud",
+            },
+            {
+                question: "What are benefits of EC2?",
+                answer: "* Scalability: new machines can be created in minutes.\n* Security: virtual machines provide stronger isolation than OS processes.\n* Developer control: developers choose the OS, software, libraries, etc.\n* Low cost: developers pay only for what they use.",
+            },
+            // VMM Implementation Overview - I.
+            {
+                question: "What distinguishes the VMM type I?",
+                answer: "In this implementation, the VMM is part of the firmware, and the operating system requires no special modifications. Each guest in this scenario has dedicated hardware, resulting in a smaller feature set compared to other types of virtualization."
+            },
+            {
+                question: "What is a challenge in implementing I/O for VMM type I?",
+                answer: "Implementing I/O is challenging because it's difficult to have enough devices and controllers to dedicate to each guest."
+            },
+            {
+                question: "How is shared I/O handled in some cases in VMM type I?",
+                answer: "In some instances, the VMM implements a control partition that runs daemons, and other guests communicate with this partition for shared I/O."
+            },
+            {
+                question: "What unique capability can VMM type I provide in terms of virtualization?",
+                answer: "This type of VMM can offer virtualization-within-virtualization, where a guest itself can act as a VMM with its own set of guests. Other types of VMMs may have difficulty achieving this."
+            },
+            // WM Implementaton Overview - II.
+            {
+                question: "Why is Type 2 VMM implementation considered less interesting from an OS perspective?",
+                answer: "Type 2 VMM implementation is less interesting from an OS perspective because there is very little OS involvement in virtualization. The VMM is treated as another process, run, and managed by the host operating system."
+            },
+            {
+                question: "How is the Type 2 VMM managed by the host in terms of OS involvement?",
+                answer: "The Type 2 VMM is simply another process run and managed by the host operating system. Even the host is unaware that it is running a VMM and virtualizing guests."
+            },
+            {
+                question: "What is a characteristic of Type 2 VMMs in terms of overall performance?",
+                answer: "Type 2 VMMs tend to have poorer overall performance compared to other virtualization approaches because they can't take full advantage of some hardware features."
+            },
+            {
+                question: "What is a benefit of Type 2 VMMs despite their potential performance limitations?",
+                answer: "A benefit of Type 2 VMMs is that they require no changes to the host operating system. This means that the host OS remains unchanged, simplifying the implementation process."
+            },
+            {
+                question: "How can a student use a Type 2 hypervisor on a native host?",
+                answer: "A student could have a Type 2 hypervisor on a native host, allowing them to run multiple guests. All of this occurs on a standard host operating system such as Windows, Linux, or MacOS."
+            }
         ],
     },
     {
