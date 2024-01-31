66b39b5ff42c0d4f42dd9b9e5ed6b347f92f0e2c
BBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-01-30 19:19:42 -0800

    Update flashcards

--- src/data.ts
+++ src/data.ts
@@ -5,14 +5,32 @@ export const topics: models.Topic[] = [
         title: "Lecture 2",
         flashcards: [
             {
-                question: "What is an operating system?",
-                answer: "Operating system is a program that acts as an intermediary between a user of a computer and the computer hardware.",
+                question: "What is an Operating System?",
+                answer: "An operating system is a program that acts as an intermediary between a user of a computer and the computer hardware."
             },
             {
-                question: "What are operating system goals?",
-                answer: `* Execute user programs and make solving user problems easier.
-* Make the computer system convenient to use.
-* Use the computer hardware in an efficient manner.`
+                question: "What are the goals of an Operating System?",
+                answer: "1. Execute user programs and make solving user problems easier.\n2. Make the computer system convenient to use.\n3. Use the computer hardware in an efficient manner."
+            },
+            {
+                question: "What are the four components of a computer system?",
+                answer: "The four components of a computer system are hardware, operating system, application programs, and users."
+            },
+            {
+                question: "What does the hardware component of a computer system provide?",
+                answer: "The hardware component provides basic computing resources, including the CPU (Central Processing Unit), memory, and I/O (Input/Output) devices."
+            },
+            {
+                question: "What is the role of the operating system in a computer system?",
+                answer: "The operating system controls and coordinates the use of hardware among various applications and users."
+            },
+            {
+                question: "How do application programs contribute to a computer system?",
+                answer: "Application programs define the ways in which the system resources are used to solve the computing problems of the users. Examples include word processors, compilers, web browsers, database systems, and video games."
+            },
+            {
+                question: "Who constitutes the 'Users' component in a computer system?",
+                answer: "Users can include people, machines, and other computers."
             },
         ],
     },
